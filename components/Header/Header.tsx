472053bf4ad440e0242d79933b61fe9b8f7a4f6b
sereja-art <sereja.artemov.art@yandex.ru>
2023-08-07 21:04:15 +0300

    features: Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -30,15 +30,22 @@ export const Header = () => {
   return (
     <>
           <header className='fixed top-0 z-50 w-full py-5 -translate-x-1/2 left-1/2'>
-        <div className='container flex gap-x-1.5 items-center mx-auto'>
-          <nav className='mx-auto w-full left-0 flex gap-x-1.5 align-middle'>
+        <div className='container flex gap-x-1.5 items-stretch mx-auto'>
+          <nav className='mx-auto w-full left-0 flex gap-x-1.5'>
             <button type='button' className='flex grow lg:grow-0 shrink-0 p-0.5 lg:p-1 border rounded-full'>
               <div className="w-full h-full gap-3 px-4 pr-1 p-0.5 rounded-full bg-slate-50 flex justify-between items-center">
-                <Link className='text-black lg:mb-[0.1em] leading-none' href='/'>Главная страница</Link>
+                <Link className='text-black lg:mb-[0.1em]' href='/'>Главная страница</Link>
                 <svg className='w-[22px] lg:w-[27px] h-auto group-hover:fill-slate-50' width="20px" height="20px" viewBox="-3.2 -3.2 38.40 38.40" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.064"></g><g id="SVGRepo_iconCarrier"> <title>arrow-right-circle</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke-width="0.00032" fill="none" fill-rule="evenodd"> <g id="Icon-Set-Filled" transform="translate(-310.000000, -1089.000000)" fill="#000000"> <path d="M332.535,1105.88 L326.879,1111.54 C326.488,1111.93 325.855,1111.93 325.465,1111.54 C325.074,1111.15 325.074,1110.51 325.465,1110.12 L329.586,1106 L319,1106 C318.447,1106 318,1105.55 318,1105 C318,1104.45 318.447,1104 319,1104 L329.586,1104 L325.465,1099.88 C325.074,1099.49 325.074,1098.86 325.465,1098.46 C325.855,1098.07 326.488,1098.07 326.879,1098.46 L332.535,1104.12 C332.775,1104.36 332.85,1104.69 332.795,1105 C332.85,1105.31 332.775,1105.64 332.535,1105.88 L332.535,1105.88 Z M326,1089 C317.163,1089 310,1096.16 310,1105 C310,1113.84 317.163,1121 326,1121 C334.837,1121 342,1113.84 342,1105 C342,1096.16 334.837,1089 326,1089 L326,1089 Z" id="arrow-right-circle"> </path> </g> </g> </g></svg>
               </div>
             </button>
+            <ul className='hidden h-auto gap-1.5 lg:flex'>
+                  { navigationRoutes.map((route, index) => (
+                      <li key={index} className="flex items-center px-6 my-0.5 border rounded-full border-gray-200/25 bg-[#2A2A2A] hover:bg-slate-50 hover:text-black">
+                       <Link className='mb-[0.1em]' href={route.route}>{route.name}</Link>
+                      </li>
+                  ))}
 
+                </ul>
             <div className='flex'>
                 {/* Кнопка меню */}
                 <div onClick={openMenu} className={`lg:hidden border rounded-full p-0.5 flex items-center`}>
@@ -46,9 +53,10 @@ export const Header = () => {
                     {isMenuOpen ? 'Закрыть' : 'Меню'}
                   </button>
                 </div>
-              {/* Меню */}
+  
+              {/* Мобильное меню */}
               { isMenuOpen && 
-                <ul className='fixed top-20 left-1/2 -translate-x-1/2 w-[90vw] h-auto p-4 py-8 overflow-auto bg-black border lg:static rounded-3xl'>
+                <ul className='fixed top-20 left-1/2 -translate-x-1/2 w-[90vw] h-auto p-4 py-8 overflow-auto bg-black border lg:hidden rounded-3xl'>
                   {navigationRoutes.map((item, index) => {
                     return (
                       <li key={index} className=''>
@@ -62,9 +70,13 @@ export const Header = () => {
 
             </div>
           </nav>
+          <div className='flex items-center justify-between gap-2 border rounded-full px-4 p-0.5'>
+            <div>1</div>
+            <div>2</div>
+          </div>
           <div>
             {/* Иконки соцсетей */}
-            <ul className='hidden gap-1 lg:flex'>
+            <ul className='items-center hidden gap-1 lg:flex'>
               <li className='border rounded-full flex items-center justify-center w-[48px] h-[48px] p-2.5'>
                 <a href="">
                   <svg className='max-w-full max-h-auto' fill="#ffffff" width="64px" height="64px" viewBox="-2.5 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>vk</title> <path d="M16.563 15.75c-0.5-0.188-0.5-0.906-0.531-1.406-0.125-1.781 0.5-4.5-0.25-5.656-0.531-0.688-3.094-0.625-4.656-0.531-0.438 0.063-0.969 0.156-1.344 0.344s-0.75 0.5-0.75 0.781c0 0.406 0.938 0.344 1.281 0.875 0.375 0.563 0.375 1.781 0.375 2.781 0 1.156-0.188 2.688-0.656 2.75-0.719 0.031-1.125-0.688-1.5-1.219-0.75-1.031-1.5-2.313-2.063-3.563-0.281-0.656-0.438-1.375-0.844-1.656-0.625-0.438-1.75-0.469-2.844-0.438-1 0.031-2.438-0.094-2.719 0.5-0.219 0.656 0.25 1.281 0.5 1.813 1.281 2.781 2.656 5.219 4.344 7.531 1.563 2.156 3.031 3.875 5.906 4.781 0.813 0.25 4.375 0.969 5.094 0 0.25-0.375 0.188-1.219 0.313-1.844s0.281-1.25 0.875-1.281c0.5-0.031 0.781 0.406 1.094 0.719 0.344 0.344 0.625 0.625 0.875 0.938 0.594 0.594 1.219 1.406 1.969 1.719 1.031 0.438 2.625 0.313 4.125 0.25 1.219-0.031 2.094-0.281 2.188-1 0.063-0.563-0.563-1.375-0.938-1.844-0.938-1.156-1.375-1.5-2.438-2.563-0.469-0.469-1.063-0.969-1.063-1.531-0.031-0.344 0.25-0.656 0.5-1 1.094-1.625 2.188-2.781 3.188-4.469 0.281-0.5 0.938-1.656 0.688-2.219-0.281-0.625-1.844-0.438-2.813-0.438-1.25 0-2.875-0.094-3.188 0.156-0.594 0.406-0.844 1.063-1.125 1.688-0.625 1.438-1.469 2.906-2.344 4-0.313 0.375-0.906 1.156-1.25 1.031z"></path> </g></svg>
