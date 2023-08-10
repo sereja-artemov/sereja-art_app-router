87b13bbaedc6a08143b68d21bda5d171ce003788
sereja-art <sereja.artemov.art@yandex.ru>
2023-08-10 20:38:49 +0300

    features: Facts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { Header } from '@/components/Header/Header'
-import Hero from '@/components/Hero/Hero'
-import Image from 'next/image'
+import Facts from '@/components/Facts/Facts';
+import { Header } from '@/components/Header/Header';
+import Hero from '@/components/Hero/Hero';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -8,10 +9,10 @@ export default function Home() {
       <Header />
       <main>
         {/* отступ для fixed header */}
-        <div className='mt-[72px] lg:mt-[88px]'></div>
+        <div className="mt-[72px] lg:mt-[88px]"></div>
         <Hero />
+        <Facts />
       </main>
     </>
-    
-  )
+  );
 }
---
+++ components/Facts/Facts.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Facts = () => {
+  return (
+    <section className="container">
+      <ul>
+        <li>5+ лет опыта</li>
+        <li>Работаю официально</li>
+        <li>Результат не гарантирую</li>
+      </ul>
+      <p>
+        Помогаю бизнесу зарабатывать с помощью современных веб-технологий и
+        маркетинговых инструментов
+      </p>
+    </section>
+  );
+};
+
+export default Facts;
