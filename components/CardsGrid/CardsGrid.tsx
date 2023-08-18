c0c43ab2946884b3eb373168810ffb7fded6dc14
sereja-art <sereja.artemov.art@yandex.ru>
2023-08-18 20:28:19 +0300

    styles: add services card background

--- components/CardsGrid/CardsGrid.tsx
+++ components/CardsGrid/CardsGrid.tsx
@@ -5,7 +5,11 @@ import CardWrapper from '../CardWrapper/CardWrapper';
 const CardsGrid = () => {
   return (
     <section className="container grid grid-flow-row-dense grid-cols-6 gap-5">
-      <CardWrapper cardLink='/services' cardTitle='Услуги' cssGridClassName='bg-[#1635A5] col-span-full lg:col-span-3'>
+      <CardWrapper
+        cardLink="/services"
+        cardTitle="Услуги"
+        cssGridClassName="bg-[#1635A5] col-span-full lg:col-span-3 line-background"
+      >
         <ServicesBlock />
       </CardWrapper>
     </section>
