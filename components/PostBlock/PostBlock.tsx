e7ac95d3650f155787bc24026f7d647da0763e99
sereja-artemov <artemov46@gmail.com>
2023-09-04 17:18:58 +0300

    features: postBlock

--- components/PostBlock/PostBlock.tsx
+++ components/PostBlock/PostBlock.tsx
@@ -17,19 +17,22 @@ export const PostBlock = () => {
         height="84"
         alt=""
       ></Image>
-      <div className="justify-between sm:flex sm:gap-4">
-        <h3 className="mb-3 text-base font-bold">Смотровая площадка</h3>
-        <div className="flex gap-3 shrink-0">
-          <span className="">
-            <AiOutlineCalendar className="w-5 h-5 fill-darkSecondary dark:fill-whiteSecondary" />
+      <div className="justify-between 2xl:flex 2xl:gap-7 items-start">
+        <h3 className="mb-3 text-base md:text-lg font-bold">Смотровая площадка</h3>
+        <div className="flex flex-wrap gap-3 shrink-0 2xl:pt-1">
+          <span className='flex gap-[.5em] text-xs items-center sm:text-sm'>
+            <AiOutlineCalendar className="w-[1.5em] h-[1.5em] fill-darkSecondary dark:fill-whiteSecondary" />
             {/* {getLocaleDate('ru', article.date, 'short')} */}
+            20.20.20
           </span>
-          <span>
-            <AiOutlineFieldTime className="w-5 h-5 fill-darkSecondary dark:fill-whiteSecondary" />
+          <span className='flex gap-[.5em] text-xs items-center sm:text-sm'>
+            <AiOutlineFieldTime className="w-[1.5em] h-[1.5em] fill-darkSecondary dark:fill-whiteSecondary" />
             {/* {article.readingTime.textRU} */}
+            20 минут
           </span>
-          <span>
-            <AiOutlineRead className="w-5 h-5 fill-darkSecondary dark:fill-whiteSecondary" />
+          <span className='flex gap-[.5em] text-xs items-center sm:text-sm'>
+            <AiOutlineRead className="w-[1.5em] h-[1.5em] fill-darkSecondary dark:fill-whiteSecondary" />
+            200 слов
             {/* {article.readingTime.words}
             {getWordEnding(article.readingTime.words, [
               ' слово',
@@ -39,8 +42,8 @@ export const PostBlock = () => {
           </span>
         </div>
       </div>
-      <hr className="my-5 h-[1px] border-t-0 bg-darkSecondary/30 dark:bg-whiteSecondary opacity-100 dark:opacity-50" />
-      <p>
+      <hr className="my-4 mt-5 lg:mt-3 h-[1px] border-t-0 bg-darkSecondary/30 dark:bg-whiteSecondary opacity-100 dark:opacity-50" />
+      <p className='md:text-base'>
         Подъем на смотровую площадку на высоту де вите иташки, откуда
         открывается потрясающий вид на город
       </p>
