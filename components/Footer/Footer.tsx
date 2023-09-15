0248030f7fc94630d16bf5b17b957594062caf1a
sereja-artemov <artemov46@gmail.com>
2023-09-15 15:43:33 +0300

    styles: footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -40,18 +40,18 @@ const Footer = () => {
                 </svg>
               </Link>
             </div>
-      <div className='flex flex-wrap gap-1'>
+      <div className='flex flex-wrap gap-1 lg:text-lg'>
         <div className='flex items-center gap-2 border border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full px-4 py-2'>
           <MdEmail />
-          <Link href='mailto:artemov46@gmail.com' target='_blank'>artemov46@gmail.com</Link>
+          <Link className='lg:mb-[2px]' href='mailto:artemov46@gmail.com' target='_blank'>artemov46@gmail.com</Link>
         </div>
         <div className='flex items-center gap-2 border border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full px-4 py-2'>
           <BsTelegram />
-          <Link href='https://t.me/sereja_art' target='_blank'>@sereja_art</Link>
+          <Link className='lg:mb-[2px]' href='https://t.me/sereja_art' target='_blank'>@sereja_art</Link>
         </div>
         <div className='flex items-center gap-2 border border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full px-4 py-2'>
           <IoLogoWhatsapp />
-          <Link href='https://wa.me/79513339923' target='_blank'>whatsapp</Link>
+          <Link className='lg:mb-[2px]' href='https://wa.me/79513339923' target='_blank'>whatsapp</Link>
         </div>
       </div>
     </footer>
