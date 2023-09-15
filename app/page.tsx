5d0210f21061ab480681147b10aabfdf15323f9b
sereja-artemov <artemov46@gmail.com>
2023-09-15 14:30:10 +0300

    features: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import CardsGrid from '@/components/CardsGrid/CardsGrid';
 import Facts from '@/components/Facts/Facts';
+import Footer from '@/components/Footer/Footer';
 import { Header } from '@/components/Header/Header';
 import Hero from '@/components/Hero/Hero';
 import Image from 'next/image';
@@ -11,6 +12,7 @@ export default function Home() {
         <Hero />
         <Facts />
         <CardsGrid />
+        <Footer />
       </main>
     </>
   );
---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+import s from './Footer.module.scss';
+import Link from 'next/link';
+import React from 'react';
+import { BsTelegram } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md';
+import { IoLogoWhatsapp } from 'react-icons/io';
+
+const Footer = () => {
+
+  return (
+    <footer className='container my-5 py-10'>
+      <div className='flex'>
+        <div className='flex items-center gap-2 border border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full px-4 py-2'>
+          <MdEmail />
+          <Link href='mailto:artemov46@gmail.com' target='_blank'>artemov46@gmail.com</Link>
+        </div>
+        <div className='flex items-center gap-2 border border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full px-4 py-2'>
+          <BsTelegram />
+          <Link href='https://t.me/sereja_art' target='_blank'>@sereja_art</Link>
+        </div>
+        <div className='flex items-center gap-2 border border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full px-4 py-2'>
+          <IoLogoWhatsapp />
+          <Link href='https://wa.me/79513339923' target='_blank'>whatsapp</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
