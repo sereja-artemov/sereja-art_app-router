fd3deafe1d4d7903ee38d121fd456715a910a2d7
sereja-art <sereja.artemov.art@yandex.ru>
2024-02-10 16:15:57 +0300

    features: add post generate og image from slug

--- app/notes/[slug]/opengraph-image.tsx
+++ app/notes/[slug]/opengraph-image.tsx
@@ -1,39 +1,41 @@
-// import { Note, allNotes } from '@/.contentlayer/generated';
-// import { notFound } from 'next/navigation';
-// import { ImageResponse } from 'next/og'
+import { getPostFromSlug } from '@/lib/getPosts';
+import { PostType } from '@/lib/types';
+import { notFound } from 'next/navigation';
+import { ImageResponse } from 'next/og'
 
-// export const runtime = 'edge'
+export const runtime = 'edge'
  
-// export const alt = ''
-// export const size = {
-//   width: 1200,
-//   height: 630,
-// }
-// export const contentType = 'image/png'
+export const alt = ''
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const contentType = 'image/png'
  
-// export default async function Image({ params }: { params: { slug: string } }) {
-//   const note: Note | undefined = allNotes.find((post) => post.slug === params.slug);
-//   if (!note) notFound()
+export default async function Image({ params }: { params: { slug: string } }) {
+
+  const  note: PostType | undefined = await getPostFromSlug(params.slug, 'notes');
+  if (!note) notFound()
  
-//   return new ImageResponse(
-//     (
-//       <div
-//         style={{
-//           fontSize: 48,
-//           background: 'white',
-//           width: '100%',
-//           height: '100%',
-//           display: 'flex',
-//           alignItems: 'center',
-//           justifyContent: 'center',
-//         }}
-//       >
-//         {note.title}
-//       </div>
-//     ),
-//     {
-//       ...size,
-//     }
-//   )
-// }
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {note.title}
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
 
